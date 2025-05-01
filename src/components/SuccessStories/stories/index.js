159d3910451e5ab241cf408a7ed6aa91b00cf772
ronental75 <ronental1975@gmail.com>
2025-05-01 09:35:46 +0300

    Initial commit

--- src/components/SuccessStories/stories/index.js
+++ src/components/SuccessStories/stories/index.js
@@ -1,10 +1,13 @@
 import story1 from './story1';
 import story2 from './story2';
-
+import story3 from './story3';
+import story4 from './story4';
 // ייצוא של כל הסיפורים כמערך
 const successStories = [
     story1,
-    story2
+    story2,
+    story3,
+    story4
     // בעתיד תוכל להוסיף עוד סיפורים כאן
   ];
 
---
+++ src/components/SuccessStories/stories/story3.js
@@ -0,0 +1,66 @@
+// קובץ: story-י.js
+
+export default {
+    id: 'story3',
+    name: 'י\'',
+    age: 65,
+    gender: 'male',
+    height: 182,
+    startWeight: 99.3,
+    currentWeight: 86.8,
+    shortDescription: 'התחיל את הליווי ביחד עם בנו ב\'. שחיין לשעבר שהחליט לחזור לכושר ולהשתפר בקצבי השחיה. הצליח לשפר משמעותית את הישגיו בבריכה.',
+    fullDescription: 'י. בן 65 התחיל את הליווי יחד עם בנו. כשחיין לשעבר, עלה במשקל בשנים האחרונות והגיע עם מטרת יעד להגיע ל-93 ק"ג ולהשתפר בקצבי השחייה. עבר תהליך רציף של ירידה במשקל בשילוב תכנית אימונים הכוללת שחייה ואימוני כוח ביתיים. י. לא רק הגיע ליעד הראשוני שלו, אלא המשיך מעבר לכך והגיע עד ל-86.8 ק"ג, ירידה של מעל 12 ק"ג, תוך שיפור משמעותי ביכולות השחייה שלו.',
+    quote: "הצלחתי לא רק לרדת במשקל מעבר ליעד המקורי שלי, אלא גם לחזור לתחושת השחייה שהכרתי בעבר. האימונים המשולבים שינו לי את הגוף והחזירו לי את היכולת התנועתית.",
+    weightData: [
+      { date: "15/12/2024", weight: 99.3, change: 0 },
+      { date: "23/12/2024", weight: 97.1, change: -2.2 },
+      { date: "30/12/2024", weight: 97.1, change: 0 },
+      { date: "06/01/2025", weight: 95.3, change: -1.8 },
+      { date: "13/01/2025", weight: 94.4, change: -0.9 },
+      { date: "20/01/2025", weight: 93.1, change: -1.3 },
+      { date: "27/01/2025", weight: 93.1, change: 0 },
+      { date: "03/02/2025", weight: 92.0, change: -1.1 },
+      { date: "10/02/2025", weight: 91.1, change: -0.9 },
+      { date: "17/02/2025", weight: 90.6, change: -0.5 },
+      { date: "24/02/2025", weight: 90.2, change: -0.4 },
+      { date: "03/03/2025", weight: 89.9, change: -0.3 },
+      { date: "10/03/2025", weight: 89.1, change: -0.8 },
+      { date: "17/03/2025", weight: 88.9, change: -0.2 },
+      { date: "24/03/2025", weight: 87.7, change: -1.2 },
+      { date: "31/03/2025", weight: 87.4, change: -0.3 },
+      { date: "07/04/2025", weight: 87.7, change: 0.3 },
+      { date: "14/04/2025", weight: 87.2, change: -0.5 },
+      { date: "21/04/2025", weight: 86.9, change: -0.3 },
+      { date: "28/04/2025", weight: 86.8, change: -0.1 }
+    ],
+    workoutData: [
+      { week: 1, date: "15/12/2024", strengthWorkouts: 1, cardioWorkouts: 4, totalWorkouts: 5, details: "שחיה: 2, כח: 1, ריצה: 2" },
+      { week: 2, date: "22/12/2024", strengthWorkouts: 2, cardioWorkouts: 4, totalWorkouts: 6, details: "שחיה: 2, כח: 2, ריצה: 2" },
+      { week: 3, date: "29/12/2024", strengthWorkouts: 3, cardioWorkouts: 5, totalWorkouts: 8, details: "שחיה: 2, כח: 3, ריצה: 3" },
+      { week: 4, date: "05/01/2025", strengthWorkouts: 2, cardioWorkouts: 5, totalWorkouts: 7, details: "שחיה: 2, כח: 2, ריצה: 3" },
+      { week: 5, date: "12/01/2025", strengthWorkouts: 2, cardioWorkouts: 4, totalWorkouts: 6, details: "שחיה: 3, כח: 2, ריצה: 1" },
+      { week: 6, date: "19/01/2025", strengthWorkouts: 3, cardioWorkouts: 3, totalWorkouts: 6, details: "שחיה: 3, כח: 3, ריצה: 0" },
+      { week: 7, date: "26/01/2025", strengthWorkouts: 3, cardioWorkouts: 3, totalWorkouts: 6, details: "שחיה: 3, כח: 3, ריצה: 0" },
+      { week: 8, date: "02/02/2025", strengthWorkouts: 2, cardioWorkouts: 3, totalWorkouts: 5, details: "שחיה: 3, כח: 2, ריצה: 0" },
+      { week: 9, date: "09/02/2025", strengthWorkouts: 2, cardioWorkouts: 3, totalWorkouts: 5, details: "שחיה: 3, כח: 2, ריצה: 0" },
+      { week: 10, date: "16/02/2025", strengthWorkouts: 3, cardioWorkouts: 3, totalWorkouts: 6, details: "שחיה: 3, כח: 2, ריצה: 0, חדר כושר: 1" },
+      { week: 11, date: "23/02/2025", strengthWorkouts: 2, cardioWorkouts: 3, totalWorkouts: 5, details: "שחיה: 3, כח: 2, ריצה: 0" },
+      { week: 12, date: "02/03/2025", strengthWorkouts: 4, cardioWorkouts: 3, totalWorkouts: 7, details: "שחיה: 3, כח: 4, ריצה: 0" },
+      { week: 13, date: "09/03/2025", strengthWorkouts: 3, cardioWorkouts: 3, totalWorkouts: 6, details: "שחיה: 3, כח: 3, ריצה: 0" },
+      { week: 14, date: "16/03/2025", strengthWorkouts: 3, cardioWorkouts: 3, totalWorkouts: 6, details: "שחיה: 3, כח: 3, ריצה: 0" },
+      { week: 15, date: "23/03/2025", strengthWorkouts: 2, cardioWorkouts: 0, totalWorkouts: 2, details: "שחיה: 0, כח: 2, ריצה: 0" },
+      { week: 16, date: "30/03/2025", strengthWorkouts: 0, cardioWorkouts: 0, totalWorkouts: 0, details: "שחיה: 0, כח: 0, ריצה: 0" },
+      { week: 17, date: "06/04/2025", strengthWorkouts: 2, cardioWorkouts: 2, totalWorkouts: 4, details: "שחיה: 2, כח: 2, ריצה: 0" },
+      { week: 18, date: "13/04/2025", strengthWorkouts: 4, cardioWorkouts: 2, totalWorkouts: 6, details: "שחיה: 2, כח: 4, ריצה: 0" },
+      { week: 19, date: "20/04/2025", strengthWorkouts: 4, cardioWorkouts: 1, totalWorkouts: 5, details: "שחיה: 1, כח: 4, ריצה: 0" }
+    ],
+   
+    image: null, // אם אין תמונה
+    highlights: [
+      'ירידה של 12.5 ק"ג (12.6% ממשקל ההתחלה)',
+      'ירידה מ-BMI 30.0 (השמנה) ל-BMI 26.2 (עודף משקל)',
+      'ביצע 101 אימונים במהלך התהליך, מתוכם 47 אימוני כוח ו-54 אימוני אירובי',
+      'עבר את יעד המשקל המקורי של 93 ק"ג והמשיך להתקדם',
+      'שיפור משמעותי בקצבי השחייה ובסיבולת הכללית'
+    ]
+  };
