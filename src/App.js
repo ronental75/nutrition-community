64ac2d32f16cb9ec2e0ae37cf2bddaa5192daaf3
ronental75 <ronental1975@gmail.com>
2025-05-02 18:01:10 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@
   import BMICalculator from './BMICalculator';
   import SuccessStories from './components/SuccessStories';
   import AccessibilityWidget from './AccessibilityWidget';
+  
 
   // import RotatingTips from './RotatingTips'
   // import ProgressTracker from './ProgressTracker'
