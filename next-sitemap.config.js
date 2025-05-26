dd33469fee71de7d22f23b5a05c0be7fab7f2b75
ronental75 <ronental1975@gmail.com>
2025-05-26 19:54:28 +0300

    Initial commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://nutrition-community.vercel.app',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+}
